679c4fdeb0aae42ca2659aae6beb371a66d09896
juju120 <43873305+juju120@users.noreply.github.com>
2019-02-14 21:43:50 -0800

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,3 @@
 <?php
-$con=mysqli_connect("localhost","root","");
-mysqli_select_db($con,"test");
-
+	$con=mysqli_connect('localhost','root','','registration');
 ?>
---
+++ reg.php
@@ -0,0 +1,17 @@
+<?php
+	require 'dbcon.php';
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: PUT,GET,POST");
+	header("Access-Control-Allow-Headers: Origin,X-Requested-With,Content-Type,Accept");
+	
+$postdata=file_get_contents("php://input");
+$request=json_decode($postdata);
+$username=mysqli_real_escape_string($con,trim($request->data->username));
+$password=mysqli_real_escape_string($con,trim($request->data->password));
+$sql="INSERT INTO `angular`(`username`, `password`) VALUES ('$username','$password')";
+if(mysqli_query($con,$sql))
+{
+	$student=['username'=>$username,'password'=>$password,'id'=>mysqli_insert_id($con)];
+	echo json_encode(['data'=>$student]);
+}
+?>
